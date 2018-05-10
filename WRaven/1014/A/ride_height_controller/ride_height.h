376ba311814d62d81a455ec7ab764f12204b4c76
DKitzman <theheretic28@gmail.com>
2018-05-09 21:57:14 -0500

    Flipped the direction

--- WRaven/1014/A/ride_height_controller/ride_height.h
+++ WRaven/1014/A/ride_height_controller/ride_height.h
@@ -2,10 +2,10 @@
 #define RIDE_HEIGHT_H
 #include <avr/io.h>
 #include <stdint.h>
-#define BACK_FORWARD_PWM 8 //PWM length of 512 microseconds
-#define BACK_BACKWARD_PWM 38 //PWM length of 2432 microseconds
-#define FRONT_FORWARD_PWM 31 //PWM length of 512 microseconds
-#define FRONT_BACKWARD_PWM 157 //PWM length of 2432 microseconds
+#define BACK_FORWARD_PWM 38 //PWM length of 512 microseconds
+#define BACK_BACKWARD_PWM 8 //PWM length of 2432 microseconds
+#define FRONT_FORWARD_PWM 157 //PWM length of 512 microseconds
+#define FRONT_BACKWARD_PWM 31 //PWM length of 2432 microseconds
 #define STOP_PWM 0
 #define DIR_STOP 0
 #define DIR_FORWARD 1

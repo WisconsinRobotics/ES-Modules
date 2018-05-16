53e12fe2e73555a47160260ba2f487a137dc2d30
Samantha Getting <sgetting@comcast.net>
2018-05-16 15:10:38 -0500

    fixed PWM stuff and some control issues

--- WRaven/1014/A/ride_height_controller/ride_height.h
+++ WRaven/1014/A/ride_height_controller/ride_height.h
@@ -6,7 +6,7 @@
 #define CMD_PING_OFF 0x02
 #define CMD_DRIVE_ACTUATORS 0x07
 #define LED_FLASH_TIME 50
-#define I2C_ADDR 0xAA
+#define I2C_ADDR 0x0A
 #define LED_PING_ON (PORTD |= (1<<PORTD0))
 #define LED_PING_OFF (PORTD &= ~(1<<PORTD0))
 #define LED_CMD_ON (PORTD |= (1<<PORTD2))
@@ -17,7 +17,7 @@
 #define BACK_FORWARD_PWM 38 //PWM length of 512 microseconds
 #define BACK_BACKWARD_PWM 8 //PWM length of 2432 microseconds
 #define FRONT_FORWARD_PWM 157 //PWM length of 512 microseconds
-#define FRONT_BACKWARD_PWM 31 //PWM length of 2432 microseconds
+#define FRONT_BACKWARD_PWM 33 //PWM length of 2432 microseconds
 #define STOP_PWM 0
 #define DIR_STOP 0
 #define DIR_FORWARD 2

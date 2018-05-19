e5ca36381a19ba96ccf73d51049cb66980cc9289
DKitzman <dkitzman@wisc.edu>
2018-05-19 17:55:13 -0500

    changed the payload structure to contain fields for both actuator directions.

--- WRaven/1014/A/ride_height_controller/ride_height.h
+++ WRaven/1014/A/ride_height_controller/ride_height.h
@@ -14,18 +14,16 @@
 #define LED_ALIVE_TOGGLE (PORTD ^= (1<<PORTD1))
 #define LED_ALIVE_FREQ 5000
 
-#define BACK_FORWARD_PWM 38 //PWM length of 512 microseconds
-#define BACK_BACKWARD_PWM 8 //PWM length of 2432 microseconds
-#define FRONT_FORWARD_PWM 157 //PWM length of 512 microseconds
-#define FRONT_BACKWARD_PWM 33 //PWM length of 2432 microseconds
+#define BACK_DOWN_PWM 38 //PWM length of 512 microseconds
+#define BACK_UP_PWM 8 //PWM length of 2432 microseconds
+#define FRONT_DOWN_PWM 157 //PWM length of 512 microseconds
+#define FRONT_FRONT_PWM 33 //PWM length of 2432 microseconds
+#define ACUTATOR_DOWN 2
+#define ACTUATOR_UP 1
 #define STOP_PWM 0
-#define DIR_STOP 0
-#define DIR_FORWARD 2
-#define DIR_BACKWARD 1
-#define BACK_ACTUATOR 0
-#define FRONT_ACTUATOR 1
-#define actuator_mask 0x03
-#define  direction_mask 0x0C
+#define back_dir_mask 0x03
+#define front_dir_mask 0x0C
+
 void pwm_init();
 void drive_actuators(uint8_t payload);
 #endif
